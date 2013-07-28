32d3c40a7a9585e1a9b4daa3074a7a631d98f3ac
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-28 10:25:19 +0900

    設定を追加

--- project/Build.scala
+++ project/Build.scala
@@ -2,11 +2,16 @@ import sbt._
 import sbt.Keys._
 
 object ConfigBuild extends Build {
-        val root = Project(
-                id = "sisioh-config",
-                base = file("."),
-                settings = Project.defaultSettings ++ Seq(
-                libraryDependencies += "com.typesafe" % "config" % "1.0.2"
-                )
-        )
+  val root = Project(
+    id = "sisioh-config",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      organization := "org.sisioh",
+      version := "0.0.12",
+      scalaVersion := "2.10.2",
+      scalacOptions ++= Seq("-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
+      javacOptions ++= Seq("-encoding", "UTF-8", "-deprecation"),
+      libraryDependencies += "com.typesafe" % "config" % "1.0.2"
+    )
+  )
 }
