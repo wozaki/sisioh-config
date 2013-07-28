005af63a09cc4b35f66aafff7b2de3d61f936806
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-28 22:43:38 +0900

    依存関係を追加

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,14 @@ object ConfigBuild extends Build {
       scalaVersion := "2.10.2",
       scalacOptions ++= Seq("-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
       javacOptions ++= Seq("-encoding", "UTF-8", "-deprecation"),
-      libraryDependencies += "com.typesafe" % "config" % "1.0.2"
+      libraryDependencies ++= Seq(
+        "org.scala-lang" % "scala-reflect" % "2.10.2",
+        "junit" % "junit" % "4.8.1" % "test",
+        "org.hamcrest" % "hamcrest-all" % "1.3" % "test",
+        "org.mockito" % "mockito-core" % "1.9.5" % "test",
+        "org.specs2" %% "specs2" % "1.14" % "test",
+        "com.typesafe" % "config" % "1.0.2"
+      )
     )
   )
 }
