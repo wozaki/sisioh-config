4f10e762349622fd4ea663ea5db83a0d844d9857
じゅんいち☆かとう <j5ik2o@gmail.com>
2013-07-31 21:25:38 +0900

    version change

--- project/Build.scala
+++ project/Build.scala
@@ -42,7 +42,7 @@ object ConfigBuild extends Build {
           </developers>
         ),
       organization := "org.sisioh",
-      version := "0.0.1",
+      version := "0.0.2-SNAPSHOT",
       scalaVersion := "2.10.2",
       scalacOptions ++= Seq("-encoding", "UTF-8", "-feature", "-deprecation", "-unchecked"),
       javacOptions ++= Seq("-encoding", "UTF-8", "-deprecation"),
